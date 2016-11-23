8b86c971cd6682164ecbee6be27122d204fb3c6d
Dazzy Ding <h@dazzyd.org>
2016-11-23 21:19:31 +0800

    Refactor online

--- online.py
+++ online.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 # coding: UTF-8
 
+import logging
+import os
+import subprocess
+import sys
+import threading
+import time
 from datetime import datetime, timedelta
 
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -23,6 +29,7 @@ class ONLINE:
     ADMIN = ['412632991', '694692391']
     TOLERANCE = timedelta(minutes=10)
     last = datetime.now()
+    notified_last = None
 
 
 @qqbot.listener((RcvdPrivateMessage, RcvdGroupMessage))
@@ -46,22 +53,51 @@ def commmand(message):
 @scheduler.scheduled_job('cron', minute='*')
 def check():
     now = datetime.now()
-    if (now - ONLINE.last) < ONLINE.TOLERANCE:
+    last = ONLINE.last
+    if (now - last) < ONLINE.TOLERANCE or ONLINE.notified_last == last:
         return
+    ONLINE.notified_last = last
     text = '\n'.join([
-        "WARNING",
-        "No message within {tolerance}.",
-        "Last message at {last}.",
-        "Send /online to check.",
-        ]).format(tolerance=ONLINE.TOLERANCE, last=ONLINE.last)
+        "** WARNING **",
+        "No message in {tolerance}. Restarting CoolQ.",
+        "If you don't receive finish message in 2 mintues, "  # No comma
+        "send /online to check or restart manually."
+        ]).format(tolerance=ONLINE.TOLERANCE)
     for qq in ONLINE.ADMIN:
         qqbot.send(SendPrivateMessage(qq=qq, text=text))
+    time.sleep(10)  # Wait for warn message sending
+    Restarter().start()
+
+
+class Restarter(threading.Thread):
+    def run(self):
+        logging.warn("Stopping CoolQ")
+        os.system("taskkill /F /T /IM CQP.exe")
+        time.sleep(10)
+        logging.warn("Starting CoolQ")
+        subprocess.Popen([
+            "C:/Users/dazzy/Desktop/CoolQ/CQP.exe",
+            "/account", "1695676191"],
+            stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        time.sleep(10)
+        logging.warn("Sending finish message")
+        text = '\n'.join([
+            "** INFO **",
+            "Restart has finished.",
+            ])
+        for qq in ONLINE.ADMIN:
+            qqbot.send(SendPrivateMessage(qq=qq, text=text))
 
 
 ################
 # __main__
 ################
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.WARNING,
+        stream=sys.stderr,
+        format="%(asctime)s %(levelname)s %(funcName)s: %(message)s",
+        )
     try:
         qqbot.start()
         scheduler.start()
