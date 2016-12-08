b255f2e64678836357eb043b8798934ad5809de3
Dazzy Ding <h@dazzyd.org>
2016-12-08 23:03:07 +0800

    Fix finish message failed to send

--- online.py
+++ online.py
@@ -38,7 +38,7 @@ def update(message):
 
 
 @qqbot.listener((RcvdPrivateMessage, ))
-def commmand(message):
+def command(message):
     if message.qq not in ONLINE.ADMIN:
         return
     if message.text != "/online":
@@ -57,30 +57,33 @@ def check():
     if (now - last) < ONLINE.TOLERANCE or ONLINE.notified_last == last:
         return
     ONLINE.notified_last = last
-    text = '\n'.join([
-        "** WARNING **",
-        "No message for {tolerance}. Restarting CoolQ.",
-        "If you don't receive finish message in 2 mintues, "  # No comma
-        "send /online to check or restart manually."
-        ]).format(tolerance=ONLINE.TOLERANCE)
-    for qq in ONLINE.ADMIN:
-        qqbot.send(SendPrivateMessage(qq=qq, text=text))
-    time.sleep(10)  # Wait for warn message sending
     Restarter().start()
 
 
 class Restarter(threading.Thread):
     def run(self):
-        logging.warn("Stopping CoolQ")
-        os.system("taskkill /F /T /IM CQP.exe")
+        # 1. Send before restart notification
+        text = '\n'.join([
+            "** WARNING **",
+            "No message for {tolerance}. Restarting CoolQ.",
+            "If you don't receive finish message in 2 mintues, "  # No comma
+            "send /online to check or restart manually."
+            ]).format(tolerance=ONLINE.TOLERANCE)
+        for qq in ONLINE.ADMIN:
+            qqbot.send(SendPrivateMessage(qq=qq, text=text))
         time.sleep(10)
-        logging.warn("Starting CoolQ")
+        # 2.1. Stop CoolQ
+        logging.warning("Stopping CoolQ")
+        os.system("taskkill /F /T /IM CQP.exe")
+        time.sleep(20)
+        # 2.2. Start CoolQ
+        logging.warning("Starting CoolQ")
         subprocess.Popen([
             "C:/Users/dazzy/Desktop/CoolQ/CQP.exe",
             "/account", "1695676191"],
             stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        time.sleep(10)
-        logging.warn("Sending finish message")
+        time.sleep(30)
+        # 3. Send after restart notification
         text = '\n'.join([
             "** INFO **",
             "Restart has finished.",
